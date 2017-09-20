6a74e0122b4ca9500a41f3ab6e93d2933d49bebf
DESKTOP-C2N6DQL\Kousuke Nakagawa <kousuke.shinan@gmail.com>
2017-09-20 14:18:31 +0900

    エネミーテストなう 追記：ガバガババックログ更新

---
+++ H29Team007/Assets/SecurityGuard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityGuard : MonoBehaviour {
+    //巡回ポイント
+    public 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
