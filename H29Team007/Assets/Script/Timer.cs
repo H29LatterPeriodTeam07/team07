c66a1848836e0a493f18bdd8582e76c11bb79c4b
mineralwater2000 <4584.ron@gmail.com>
2017-10-20 16:26:51 +0900

    タイマーをとりあえず動くくように更新

---
+++ H29Team007/Assets/Script/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    Slider m_slider;
+    float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        m_slider = GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        //タイムアップ
+        if(timer > m_slider.maxValue)
+        {
+            timer = 0.0f;
+            //リザルト画面表示
+        }
+
+        m_slider.value = timer;
+	}
+}
