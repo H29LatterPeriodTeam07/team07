3c7654be2ee7eec4fc7e5c30aa9983772d8d2b27
DESKTOP-C2N6DQL\Kousuke Nakagawa <kousuke.shinan@gmail.com>
2017-10-27 16:20:08 +0900

    特売品にモデルを入れた。あと特売品をエディタ上で操作して出せるようになった

--- H29Team007/Assets/Script/SaleSpown.cs
+++ H29Team007/Assets/Script/SaleSpown.cs
@@ -4,53 +4,45 @@ using UnityEngine;
 
 public class SaleSpown : MonoBehaviour {
 
-    //ランダムに出現させる特売品のプレハブ
-    public GameObject[] m_SaleAnimals;
+    //上から順番に出現させる特売品のプレハブ
+    [SerializeField, TooltipAttribute("上から順に出現する特売品のプレハブを入れる(ApperTimeと要素数は同じにするように)")]
+    private GameObject[] m_SaleAnimals;
     //出現時間
+    [SerializeField, TooltipAttribute("指定した特売品が出現する時間の指定(float型)")]
     public float[] m_ApperTime;
-    //ここから何匹出現するか
-    [SerializeField, Header("何匹出現させる？")]
-    public int m_Total;
     public GameObject m_Time;
-    public Queue<GameObject> m_Sale;
-    //何匹出現したか
     private int m_Num = 0;
-    private float m_CurrentTime = 0.0f;
-    private int m_number;
     Timer m_timer;
+    int m_CurrentSaleAnimeIndex=0;
+    int m_CurrentApperTimeIndex=0;
 
     // Use this for initialization
     void Start()
     {
-        m_number = Random.Range(0, m_SaleAnimals.Length);
         m_timer = m_Time.GetComponent<Timer>();
-        for(int i =0; m_SaleAnimals.Length > i; i++)
-        {
-            
-        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; m_ApperTime.Length > i; i++)
+        if (m_ApperTime.Length > m_CurrentApperTimeIndex)
         {
-            if (m_timer.timer > m_ApperTime[i])
-            {
+            if (m_timer.timer > m_ApperTime[m_CurrentApperTimeIndex])
+            { 
+                m_CurrentApperTimeIndex++;
                 Appear();
                 m_Num++;
-
             }
         }
     }
 
     void Appear()
     {
-        if (m_Num < m_Total)
+        if (m_Num < m_SaleAnimals.Length)
         {
-            Instantiate(m_SaleAnimals[m_number], transform.position, transform.rotation);
+            Instantiate(m_SaleAnimals[m_CurrentSaleAnimeIndex], transform.position, transform.rotation);
+            m_CurrentSaleAnimeIndex++;
         }
-        m_CurrentTime = 0.0f;
     }
 }
