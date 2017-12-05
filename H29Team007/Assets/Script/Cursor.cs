60b4c6514ffcf39e3c178289070a2d7b57fa27b2
mineralwater2000 <4584.ron@gmail.com>
2017-12-05 12:10:43 +0900

    Titleシーン関連の追加

---
+++ H29Team007/Assets/Script/Cursor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Cursor : MonoBehaviour {
+
+    private RectTransform m_RectTranslate;
+
+    [SerializeField]
+    private AnimationCurve _fadeCurve = null;
+    private float _curveRate = 0;
+    private float _fadingSpeed = 0.05f;
+
+    //Color selectedColor;
+
+    // Use this for initialization
+    void Start () {
+        m_RectTranslate = GetComponent<RectTransform>();
+        /////selectedColor = .GetComponent<Image>().color;
+
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        GameObject selectedObject = EventSystem.current.currentSelectedGameObject;
+        Color selectedColor = selectedObject.GetComponent<Image>().color;
+
+        if (selectedObject == null)
+        {
+            return;
+        }
+
+        Vector2 cursorPosition = new Vector2(-230, 0);
+
+        m_RectTranslate.anchoredPosition = selectedObject.GetComponent<RectTransform>().anchoredPosition + cursorPosition;
+
+
+        Color alpha = selectedColor;
+        alpha.a = _fadeCurve.Evaluate(_curveRate);
+        selectedColor = alpha;
+
+        if (_curveRate == 0 || _curveRate == 1f)
+        {
+            _fadingSpeed *= -1;
+        }
+
+    }
+}
---
+++ H29Team007/Assets/Script/Title_tenmetu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Title_tenmetu : MonoBehaviour
+{
+    public GameObject _Button;
+
+    private Image _image;
+    private Text _text;
+
+    //どのようにフェードするかのカーブ
+    [SerializeField]
+    private AnimationCurve _fadeCurve = null;
+
+    //カーブのどこにいるかの割合(0~1)
+    private float _curveRate = 0;
+
+    //フェードする速度
+    private float _fadingSpeed = 0.05f;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+        _text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        //アニメーション
+        _curveRate = Mathf.Clamp(_curveRate + _fadingSpeed, 0f, 1f);
+
+        //Color textColor = _text.color;
+        //textColor.a = _fadeCurve.Evaluate(_curveRate);
+        //_text.color = textColor;
+
+        Color imageColor = _image.color;
+        imageColor.a = _fadeCurve.Evaluate(_curveRate);
+        _image.color = imageColor;
+
+        //アニメーション反転
+        if (_curveRate == 0 || _curveRate == 1f)
+        {
+            _fadingSpeed *= -1;
+        }
+
+        if (Input.GetButtonDown("Submit") || Input.GetButtonDown("XboxA"))
+        {
+            this.gameObject.SetActive(false);
+            _Button.SetActive(true);
+        }
+    }
+
+}
