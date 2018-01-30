9886e73e7a80224c38205cfdb6f14e316ab40608
DESKTOP-C2N6DQL\Kousuke Nakagawa <kousuke.shinan@gmail.com>
2018-01-30 15:51:41 +0900

    マザー

--- H29Team007/Assets/Script/Enemy/Parent.cs
+++ H29Team007/Assets/Script/Enemy/Parent.cs
@@ -13,10 +13,6 @@ public class Parent : MonoBehaviour
     }
     //巡回ポイント
     public Transform[] m_PatrolPoints;
-    //見える距離
-    float m_ViewingDistance;
-    //視野角
-    float m_ViewingAngle;
 
     private ParentState m_State = ParentState.NormalMode;
     private Animator m_Animator;
@@ -29,6 +25,7 @@ public class Parent : MonoBehaviour
     Transform m_PlayerLookpoint;
     //自身の目の位置
     Transform m_EyePoint;
+    int m_rand;
 
 
     // Use this for initialization
@@ -52,8 +49,6 @@ public class Parent : MonoBehaviour
         //巡回中
         if (m_State == ParentState.NormalMode)
         {
-            m_ViewingDistance = 100;
-            m_ViewingAngle = 45;
             m_Agent.speed = 1.0f;
          //   SetNewPatrolPointToDestination();
             if (HasArrived())
@@ -66,65 +61,13 @@ public class Parent : MonoBehaviour
     //次の巡回ポイントを目的地に設定する
     void SetNewPatrolPointToDestination()
     {
-        m_CurrentPatrolPointIndex
-            = (m_CurrentPatrolPointIndex + 1) % m_PatrolPoints.Length;
-
-        m_Agent.destination = m_PatrolPoints[m_CurrentPatrolPointIndex].position;
+        m_rand = Random.Range(0, m_PatrolPoints.Length);
+        m_Agent.destination = m_PatrolPoints[m_rand].transform.position;
     }
 
     // 目的地に到着したか
     bool HasArrived()
     {
         return (Vector3.Distance(m_Agent.destination, transform.position) < 0.5f);
-    }
-
-    //プレイヤーが見える距離内にいるか？
-    bool IsPlayerInViewingDistance()
-    {
-        //自身からプレイヤーまでの距離
-        float distanceToPlayer = Vector3.Distance(m_PlayerLookpoint.position, m_EyePoint.position);
-        //プレイヤーが見える距離内にいるかどうかを返却する
-        return (distanceToPlayer <= m_ViewingDistance);
-    }
-
-    //プレイヤーが見える視野角内にいるか？
-    bool IsPlayerInViewingAngle()
-    {
-        //自分からプレイヤーへの方向ベクトル(ワールド座標系)
-        Vector3 directionToPlayer = m_PlayerLookpoint.position - m_EyePoint.position;
-        // 自分の正面向きベクトルとプレイヤーへの方向ベクトルの差分角度
-        float angleToPlayer = Vector3.Angle(m_EyePoint.forward, directionToPlayer);
-
-        // 見える視野角の範囲内にプレイヤーがいるかどうかを返却する
-        return (Mathf.Abs(angleToPlayer) <= m_ViewingAngle);
-    }
-
-    // プレイヤーにRayを飛ばしたら当たるか？
-    bool CanHitRayToPlayer()
-    {
-        // 自分からプレイヤーへの方向ベクトル（ワールド座標系）
-        Vector3 directionToPlayer = m_PlayerLookpoint.position - m_EyePoint.position;
-        // 壁の向こう側などにいる場合は見えない
-        RaycastHit hitInfo;
-        bool hit
-            = Physics.Raycast(m_EyePoint.position, directionToPlayer, out hitInfo);
-        // プレイヤーにRayが当たったかどうかを返却する
-        return (hit && hitInfo.collider.tag == "Player");
-    }
-
-    // プレイヤーが見えるか？
-    bool CanSeePlayer()
-    {
-        // 見える距離の範囲内にプレイヤーがいない場合→見えない
-        if (!IsPlayerInViewingDistance())
-            return false;
-        // 見える視野角の範囲内にプレイヤーがいない場合→見えない
-        if (!IsPlayerInViewingAngle())
-            return false;
-        // Rayを飛ばして、それがプレイヤーに当たらない場合→見えない
-        if (!CanHitRayToPlayer())
-            return false;
-        // ここまで到達したら、それはプレイヤーが見えるということ
-        return true;
     }
 }
